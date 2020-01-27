f54379d53d83770f8c45a29037d5106d22216ffb
Catherine <cbain1@butler.edu>
2020-01-27 11:28:23 -0500

    Mon. Jan 27

---
+++ classLists.py
@@ -0,0 +1,22 @@
+import sys
+import random
+
+# Define main for loops, bro
+def main():
+    
+    c = [10,20,30,40]
+    print(c)
+
+    d = ['frog','cat','taco','burrito']
+    print(d)
+
+    listception = [c,d,c]
+    print(listception)
+
+    listlistception = [listception]
+    print(listlistception)
+
+
+
+if __name__ == '__main__':
+    main()
