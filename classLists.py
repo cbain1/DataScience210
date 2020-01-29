1fe223f2765f69c60a2938f4ce964f4cd0d0005a
Catherine <cbain1@butler.edu>
2020-01-29 08:55:28 -0500

    more on lists

--- classLists.py
+++ classLists.py
@@ -16,7 +16,22 @@ def main():
     listlistception = [listception]
     print(listlistception)
 
+    #lits dont have to have the same stuff in it
+    f = ['spam', 2.5, [c]]
+    print(f)
+    empty = [] #look an empty list
 
+    c[1] = 'orange'
+    print(c) #look i changed the entry in the first position of the list
+
+    #Boolean on lists
+    print(5 in f)
+    print(20 in f)
+    print('spam')
+
+    #print all of list c do this.... or just print c
+    for x in c:
+        print(x)
 
 if __name__ == '__main__':
     main()
